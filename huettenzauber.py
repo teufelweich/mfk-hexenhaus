f6fd51e3f6ba6fc1d549c7663ff40b6475c04ca2
Marius Teufelweich <33784413+teufelweich@users.noreply.github.com>
2023-07-26 15:59:49 +0200

    fixed playback finish detection

--- huettenzauber.py
+++ huettenzauber.py
@@ -7,7 +7,7 @@ from pprint import pprint
 import asyncio
 from typing import Tuple
 
-from aio_mpv_jsonipc import MPV
+from python_mpv_jsonipc import MPV
 
 CONFIG = None
 with open ('pyconfig.toml', 'rb') as f:
@@ -35,20 +35,23 @@ def parse_steps(steps: str) -> Tuple:
         assert len(s) == 3, f'need 3 intervals not {steps}'
         return [int(i) for i in s]
 
-async def play_video(mpv: MPV, clip_path: str, clip_name: str):
+def play_video(mpv: MPV, clip_path: str, clip_name: str):
     print('stop current video output and play new clip', clip_name)
     # stops video and clears playlist
-    await mpv.send(['stop'])
+    # await mpv.send(['stop'])
+    mpv.command('stop')
 
     # add files to playlist
-    await mpv.send(['loadfile', CONFIG['file_locations']['TV_ON_VIDEO'], 'append-play'])
-    await mpv.send(['loadfile', clip_path, 'append'])
+    # await mpv.send(['loadfile', CONFIG['file_locations']['TV_ON_VIDEO'], 'append-play'])
+    # await mpv.send(['loadfile', clip_path, 'append'])
+    mpv.command('loadfile', CONFIG['file_locations']['TV_ON_VIDEO'], 'append-play')
+    mpv.command('loadfile', clip_path, 'append')
 
 async def play_wled(command):
     try:
         print('send command to wled')
         # TODO send command to config['led']['wled_url]
-        asyncio.sleep(1)
+        await asyncio.sleep(1)
     finally:
         # TODO turn wled off
         print('turn wled off')
@@ -57,18 +60,18 @@ async def play_fog(steps):
     try:
         # wait for the initial delay until starting fog
         print('wait until FOG ON for', steps[0], 's')
-        asyncio.sleep(steps[0])
+        await asyncio.sleep(steps[0])
 
         while True:
             # TODO turn fog on
             print('FOG ON for', steps[1], 's')
             # keep fog on for steps[1] seconds
-            asyncio.sleep(steps[1])
+            await asyncio.sleep(steps[1])
 
             # TODO turn fog off
             print('FOG OFF for', steps[2], 's')
             # keep fog off for steps[2] seconds
-            asyncio.sleep(steps[2])
+            await asyncio.sleep(steps[2])
 
     finally:
         # TODO turn fog off
@@ -79,18 +82,18 @@ async def play_water(steps):
     try:
         # wait for the initial delay until starting water
         print('wait until WATER ON for', steps[0], 's')
-        asyncio.sleep(steps[0])
+        await asyncio.sleep(steps[0])
 
         while True:
             # TODO turn water on
             print('WATER ON for', steps[1], 's')
             # keep water on for steps[1] seconds
-            asyncio.sleep(steps[1])
+            await asyncio.sleep(steps[1])
 
             # TODO turn water off
             print('WATER OFF for', steps[2], 's')
             # keep water off for steps[2] seconds
-            asyncio.sleep(steps[2])
+            await asyncio.sleep(steps[2])
 
     finally:
         # TODO turn water off
@@ -98,9 +101,10 @@ async def play_water(steps):
 
 async def run_scene(mpv, scene):
     background_tasks = set()
+    print(f'\n-----------------\nstart playing scene {scene["clip_name"]} with wled_cmd={scene["wled_command"]} fog_steps={scene["fog_steps"]} water_steps={scene["water_steps"]}')
     try:
         # play clip on screen
-        await play_video(mpv, scene['clip_path'], scene['clip_name'])
+        play_video(mpv, scene['clip_path'], scene['clip_name'])
 
         # create concurrent tasks
         fog_steps = parse_steps(scene['fog_steps'])
@@ -113,16 +117,32 @@ async def run_scene(mpv, scene):
             water_task = asyncio.create_task(play_water(water_steps), name='water_task')
             background_tasks.add(water_task)
 
+        # give mpv time to load video and start playing before checking for scene end
+        await asyncio.sleep(5)
+        # wait until callback was called
+        while True:
+            if mpv.idle_active: # idle means playing has finished so we stop the scene
+                print('currently idling, so finished playing')
+                break
+
+            remaining_time = mpv.playtime_remaining
+            if not remaining_time:
+                await asyncio.sleep(1)
+                continue
+            print(f'remaining {remaining_time}s to play')
+            await asyncio.sleep(mpv.playtime_remaining / 2)
+
     finally:
         for task in background_tasks:
+            print('cancelling task', task.get_name())
             task.cancel()
 
 
 async def main():
     args = parse_args()
     # Use MPV that is running and connected to /tmp/mpv-socket.
-    mpv = MPV(socket=f"/tmp/mpv-socket-{args.screen}")
-    await mpv.start()
+    mpv = MPV(start_mpv=False, ipc_socket=f"/tmp/mpv-socket-{args.screen}")
+    # await mpv.start()
     # print('get volume')
     # response = await mpv.send(['get_property','ao-volume'])
     # print('audio volume', response)
@@ -137,6 +157,7 @@ async def main():
         scene = random.choice(SCENE_CSV)
 
         await run_scene(mpv, scene)
+        await asyncio.sleep(5)
     
 
 
